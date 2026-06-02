643b9f9a045a93ae90a822d8bd91fa7364885a85
Saurabh Harankhedkar <sourabh.a.h.30@gmail.com>
2026-06-01 23:45:21 -0400

    Updated Comments

--- agent/utils/connect_llm.py
+++ agent/utils/connect_llm.py
@@ -2,7 +2,7 @@ from langchain_ollama import ChatOllama
 
 
 def get_llm():
-
+    """Utility function to get the LLM instance."""
     llm = ChatOllama(
         model="qwen3:8b",
         temperature=0,
